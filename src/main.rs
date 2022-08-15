1f9fc830dd2984c5a58538c45e6e7257916af59d
Manoel <manoel.lobo@gmail.com>
2022-08-15 20:08:18 -0300

    feat: response (header + html)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{BufRead, BufReader, Write},
+    fs,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
@@ -16,13 +17,17 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
 
-    let http_request: Vec<_> = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status = "HTTP/1.1 200 OK";
+    let content = fs::read_to_string("hello.html").unwrap();
+    let length = content.len();
+
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
