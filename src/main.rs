6392220022eb3a053bd4952ab062870ebc74b46a
Manoel <manoel.lobo@gmail.com>
2022-08-15 20:21:43 -0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,15 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status = "HTTP/1.1 200 OK";
-        let content = fs::read_to_string("hello.html").unwrap();
-        let length = content.len();
-
-        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status, content_file) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status = "HTTP/1.1 404 NOT FOUND";
-        let content = fs::read_to_string("404.html").unwrap();
-        let length = content.len();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+    let content = fs::read_to_string(content_file).unwrap();
+    let length = content.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
